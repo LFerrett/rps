997321dde57c0213fba57fd44eadcf4dfcf7ee5b
Lauren Ferrett <LFerrett@gmail.com>
2021-12-08 14:04:42 -0500

    Committed mostly final game, but a broken loop to fix later.

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var computerHand = ["R", "P", "S"];
 
 // console.log(computerSelector);
 
-var userHand = prompt("Rock, Paper, or Scissors? Input R P or S to play");
+var userHand = prompt("Rock, Paper, or Scissors? Input R, P, or S to play");
+userHand = userHand.toUpperCase();
 var userScore = 0;
 var computerScore = 0;
 
@@ -12,36 +13,42 @@ var computerScore = 0;
 //     console.log(userHand);
 //     console.log(computerHand[computerSelector]);
 // }
-
+function playGame() {
 if (userHand != null) {
     if (userHand === computerHand[computerSelector]) {
-        alert("You tied! " + "Your score is " + userScore + "Computer score is " + computerScore);
+        alert("You tied! " + "Your score is " + userScore + " Computer score is " + computerScore);
        // console.log("You tied!");
        // console.log(userScore, computerScore)
     } else if (userHand === "R" && computerHand[computerSelector] === "P") {
         computerScore = +1;
-        alert("Computer won! " + "Your score is " + userScore + "Computer score is " + computerScore);
+        alert("Computer won! " + "Your score is " + userScore + " Computer score is " + computerScore);
         // console.log(userScore, computerScore)
     } else if (userHand === "R" && computerHand[computerSelector] === "S") {
         userScore = +1;
-        alert("User won!" + "Your score is " + userScore + "Computer score is " + computerScore);
+        alert("User won!" + "Your score is " + userScore + " Computer score is " + computerScore);
         // console.log(userScore, computerScore)
     } else if (userHand === "S" && computerHand[computerSelector] === "P") {
         userScore = +1;
-        alert("User won!" + "Your score is " + userScore + "Computer score is " + computerScore);
+        alert("User won!" + "Your score is " + userScore + " Computer score is " + computerScore);
         // console.log(userScore, computerScore)
     } else if (userHand === "S" && computerHand[computerSelector] === "R") {
         computerScore = +1;
-        alert("Computer won!" + "Your score is " + userScore + "Computer score is " + computerScore);
+        alert("Computer won!" + "Your score is " + userScore + " Computer score is " + computerScore);
         // console.log(userScore, computerScore)
     } else if (userHand === "P" && computerHand[computerSelector] === "S") {
         computerScore = +1;
-        alert("Computer won!" + "Your score is " + userScore + "Computer score is " + computerScore);
+        alert("Computer won!" + "Your score is " + userScore + " Computer score is " + computerScore);
         // console.log(userScore, computerScore)
     } else if (userHand === "P" && computerHand[computerSelector] === "R") {
         userScore = +1;
-        alert("User won!" + "Your score is " + userScore + "Computer score is " + computerScore);
+        alert("User won!" + "Your score is " + userScore + " Computer score is " + computerScore);
         // console.log(userScore, computerScore)
         }
+        var playAgain = confirm("Do you want to play again?");
+    if (playAgain) {
+        playGame()
+}
     }
-
+    
+}
+playGame();
